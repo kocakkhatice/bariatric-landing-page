64254647d66f689db70b2f1e66c03f9e7fb13184
Hatice Koçak <30705864+kocakkhatice@users.noreply.github.com>
2022-04-12 12:06:43 +0300

    add about for mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header";
-
+import About from "./components/About";
 export default function App() {
   return (
     <div className="App relative">
       <Header />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,36 @@
+export default function About() {
+  return (
+    <div className="container px-6 pb-12">
+      <div className="flex  flex-col justify-center items-center text-center">
+        <h1 className="font-['Satisfy'] text-4xl">
+          The Best Tasting Protein? It's Inspire!
+        </h1>
+        <p
+          className=" color-[#2b2b2b] leading-relaxed mb-4"
+          style={{ fontFamily: "Montesarrat, sans-serif" }}
+        >
+          We're the Original and the Best! For years, Bariatric Eating has been
+          formulating lip smacking flavors for your post op palate. Our products
+          are GMP quality that meet or surpass supplement guidelines of the
+          ASMBS and top bariatric centres. We don’t repurpose cheap body builder
+          protein and 1990's diet doctor food and tell you it's special. We
+          develop our own products - that's why we are number one.
+        </p>
+        <p
+          className="font-bold mb-4 leading-relaxed"
+          style={{ fontFamily: "Montesarrat, sans-serif" }}
+        >
+          We’ve had bariatric surgery and rely on Inspire & Journey to maintain
+          our weight loss, have healthy hair & teeth and feel great!
+        </p>
+        <a
+          href="#"
+          className="text-[#1dafd9] underline underline-offset-4 decoration-[#1dafd99] leading-relaxed"
+          style={{ fontFamily: "Montesarrat, sans-serif" }}
+        >
+          Our Story
+        </a>
+      </div>
+    </div>
+  );
+}
